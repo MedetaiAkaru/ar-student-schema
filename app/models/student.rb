9c55f4ac414d7830035bc5c0b3e0a72a8cb79482
Joel Lim <akaru@littlix.com>
2015-12-18 09:41:28 +0800

    Validation Magic

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,10 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+validates :email, :format => { :with => /[a-z]+@[a-z]+\.[a-z]{2,}/, message: "Email invalid" }
+validates :email, :uniqueness => true
+validates :age, :numericality => { :greater_than => 5 }
+
 	def name
 		return self.first_name + " " + self.last_name
 	end
@@ -10,4 +14,5 @@ class Student < ActiveRecord::Base
 		now = Date.today
     return age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
 	end
+
 end
